d484f6e00c266e3e64f3cf8c396ae5830c446630
Gonza1997 <diazgonzalo2@live.com.ar>
2017-02-07 14:32:24 -0300

    Add files via upload

---
+++ jsEntradaSalida-10.js
@@ -0,0 +1,15 @@
+/*Debemos lograr tomar el importe por ID.
+Transformarlo a entero (parseInt), luego
+mostrar el importe con un Descuento del 25 %
+en el cuadro de texto "RESULTADO"*/
+function MostrarAumento()
+{
+	var sueldo;
+	var resultado;
+	sueldo = document.getElementById('importe').value;
+	sueldo = parseInt(sueldo);
+	resultado = sueldo *0.75;
+	resultado = parseInt(resultado);
+	document.getElementById('resultado').value = resultado;
+
+}
