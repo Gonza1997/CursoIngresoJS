9524f3f86a97a08440954a4238e75874f01a849d
Gonza1997 <diazgonzalo2@live.com.ar>
2017-02-08 20:36:38 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,26 @@
 function Mostrar()
+
 {
-//tomo la edad  
+	var edad;
+	edad = document.getElementById('edad').value;
+	edad = parseInt(edad);
 
+	if(edad>17)
+	{
+		alert("Usted es un adulto.");
+	}
 
+	else
+	{
+		if(edad<13)
+		{
+			alert("Usted es un niño.");
+		}
 
+		else
+		{
+			alert("Usted es un adolescente.");
+		}
+	}
 
-}//FIN DE LA FUNCIÓN
+}	
