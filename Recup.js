e91a04f00f1b1769766f5add81fc8a544fd86e33
Gonza1997 <diazgonzalo2@live.com.ar>
2017-02-21 11:09:50 -0300

    Add files via upload

---
+++ Recup.js
@@ -0,0 +1,16 @@
+var numero;
+var perimetro;
+numero = document.getElementById('NumeroID').value;
+perimetro = numero * 4; 
+alert("El perímetro es: "+perimetro);
+
+var importe;
+importe = prompt("Ingrese el importe.");
+importe = parseInt(importe);
+importefinal = importe * 1.21;
+alert("El importe final es:"+importefinal);
+
+var ancho;
+var largo;
+ancho = prompt("Ingrese el ancho del terreno.");
+largo = prompt("Ingrese el largo del terreno.");
