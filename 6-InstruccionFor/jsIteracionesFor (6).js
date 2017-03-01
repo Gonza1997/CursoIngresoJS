8e77756f5dfd9340d1f38b5ac00ad6636e6f9546
Gonza1997 <diazgonzalo2@live.com.ar>
2017-03-01 11:50:58 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,7 +1,63 @@
 function Mostrar()
-{
+{ //Ejercicio Nº8 del parcial
+	var numero;
+	var contador = 0;
+	var respuesta = true;
+	var contadorPar = 0;
+	var promedio = 0;
+	var sumanumeros = 0;
+	var maximo;
+	var minimo;
+	
+	while(respuesta == true )
+	{
+		numero = prompt("Ingrese un número: ");
+		numero = parseInt(numero);
+
+		while(numero < 0)
+		{
+			numero = prompt("Ingrese un número positivo: ");
+			numero = parseInt(numero);
+		}
+		
+
+		//Finalización de toma de datos.
+
+		if(numero % 2 == 0)
+		{
+			contadorPar++;
+		}
+
+		if(contador == 0)
+		{
+			maximo = numero;
+			minimo = numero;
+		}
+
+		else
+		{
+			if(numero<minimo)
+			{
+				minimo = numero;
+			}
+			else
+			{
+				maximo = numero;
+			}
+		}
+
+		contador++;
 
 
+		sumanumeros = sumanumeros + numero;
+		respuesta = confirm("¿Desea seguir introduciendo números?");
+	}
 
+	promedio = sumanumeros / contador;
 
-}//FIN DE LA FUNCIÓN
+	document.write("</br>La cantidad de números par es: "+contadorPar);
+	document.write("</br>El promedio de los números es: "+promedio);
+	document.write("</br>La suma de los números es: "+sumanumeros);
+	document.write("</br>El máximo de los números es: "+maximo);
+	document.write("</br>El mínimo de los números es: "+minimo);
+}
--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,7 +1,28 @@
 function Mostrar()
-{
+{ //Sacar número perfecto
+	var numero;
+	var numeroanterior;
+	var numerorecorrido;
+	var acumDiv = 0;
 
+	numero = prompt("ingrese su número: ");
+	numero = parseInt(numero);
 
+	for(numeroanterior = 1; numerorecorrido<=numero;numeroanterior++)
+	{
+		for(numerorecorrido = 1; numerorecorrido<=numero; numerorecorrido++)
+		{
+			if(numero % numerorecorrido == 0)
+			{
+				acumDiv = acumDiv + numerorecorrido;
+			}
+		}
+	}
+			
 
-
-}//FIN DE LA FUNCIÓN
+	
+	if(acumDiv == numero)
+			{
+				console.log(numero+" es perfecto.");
+			}
+}
